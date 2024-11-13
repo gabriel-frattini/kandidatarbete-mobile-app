11e34ff457c6eac8bba0971e82818770d4bb571f
saadih <120604473+saadih@users.noreply.github.com>
2024-11-13 13:36:12 +0100

    Feat. [Views] Added Exam view

---
+++ app/src/main/java/com/example/dat068_tentamina/Exam.kt
@@ -0,0 +1,78 @@
+package com.example.dat068_tentamina
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Exam() {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "exam",
+            fontSize = 25.sp,
+            lineHeight = 25.sp,
+            textAlign = TextAlign.Right,
+            modifier = Modifier
+                .padding(20.dp)
+                .align(alignment = Alignment.End)
+        )
+        Text(
+            text = "Welcome to the exam.",
+            fontSize = 50.sp,
+            lineHeight = 80.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+        Row (
+
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        {
+        }
+        ElevatedButton(
+            onClick = { ready() },
+            colors = ButtonColors(Color.DarkGray, Color.White, Color.LightGray, Color.LightGray),
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(10.dp)
+                .requiredHeight(75.dp)
+                .requiredWidth(250.dp)
+
+        ) {
+        }
+
+    }
+}
