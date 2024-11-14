a411699a0a61ffbd19943e7322df7c4f7d14a3ff
Che Tran <chelongtran@gmail.com>
2024-11-14 19:08:34 +0100

    you can now draw

--- app/src/main/java/com/example/dat068_tentamina/MainActivity.kt
+++ app/src/main/java/com/example/dat068_tentamina/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.dat068_tentamina.ui.theme.DAT068TentaminaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DAT068TentaminaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DrawingScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DrawingScreen() {
+    val lines = remember { mutableStateListOf<Line>() }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DAT068TentaminaTheme {
-        Greeting("Android")
+    androidx.compose.foundation.Canvas(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectDragGestures { change, dragAmount ->
+                change.consume()
+                lines.add(Line(
+                    start = change.position - dragAmount,
+                    end = change.position
+                ))
+            }
+        }
+
+    ) {
+        lines.forEach { line ->
+            drawLine(
+                start = line.start,
+                end = line.end,
+                color = line.color,
+                strokeWidth = line.strokeWidth.toPx(),
+                cap = StrokeCap.Round
+            )
+        }
     }
 }
+
+data class Line(
+    val start: Offset,
+    val end: Offset,
+    val color: Color = Color.Black,
+    val strokeWidth: Dp = 1.dp
+)
