e34553a00e14878148cb1f8f899546a9bd222e6e
Che Tran <chelongtran@gmail.com>
2024-11-15 11:19:11 +0100

    Refactor. [backend]. Created a folder with model classes and added Overlay.kt to it

---
+++ app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
@@ -0,0 +1,48 @@
+package com.example.dat068_tentamina.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.dat068_tentamina.viewmodel.TentaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Overlay(viewModel: TentaViewModel) {
+    Column {
+        TopAppBar(
+            title = { Text("Simple TopAppBar") },
+            navigationIcon = {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Filled.Menu, contentDescription = null)
+                }
+            },
+            actions = {
+                // RowScope here, so these icons will be placed horizontally
+                IconButton(onClick = {
+                    viewModel.pop()
+                }) {
+                    Icon(Icons.Filled.ArrowBack, contentDescription = "Localized description")
+                }
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(Icons.Filled.ArrowForward, contentDescription = "Localized description")
+                }
+            }
+        )
+        Box(modifier = Modifier.weight(1f).fillMaxSize()) {
+            DrawingScreen(viewModel)
+        }
+    }
+
+}
