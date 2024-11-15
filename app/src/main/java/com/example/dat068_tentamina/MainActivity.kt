2d4602ac71cd7089734ba16d58834ed8a2fc7424
Che Tran <chelongtran@gmail.com>
2024-11-15 11:23:22 +0100

    Refactor. [backend]. Uses the refactor

--- app/src/main/java/com/example/dat068_tentamina/MainActivity.kt
+++ app/src/main/java/com/example/dat068_tentamina/MainActivity.kt
@@ -4,20 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.example.dat068_tentamina.ui.theme.DAT068TentaminaTheme
+import com.example.dat068_tentamina.ui.Overlay
+import com.example.dat068_tentamina.viewmodel.TentaViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,44 +14,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DAT068TentaminaTheme {
-                DrawingScreen()
+                val tentaViewModel = TentaViewModel()
+                Overlay(tentaViewModel)
             }
         }
     }
 }
 
-@Composable
-fun DrawingScreen() {
-    val lines = remember { mutableStateListOf<Line>() }
 
-    androidx.compose.foundation.Canvas(modifier = Modifier
-        .fillMaxSize()
-        .pointerInput(Unit) {
-            detectDragGestures { change, dragAmount ->
-                change.consume()
-                lines.add(Line(
-                    start = change.position - dragAmount,
-                    end = change.position
-                ))
-            }
-        }
 
-    ) {
-        lines.forEach { line ->
-            drawLine(
-                start = line.start,
-                end = line.end,
-                color = line.color,
-                strokeWidth = line.strokeWidth.toPx(),
-                cap = StrokeCap.Round
-            )
-        }
-    }
-}
 
-data class Line(
-    val start: Offset,
-    val end: Offset,
-    val color: Color = Color.Black,
-    val strokeWidth: Dp = 1.dp
-)
+
