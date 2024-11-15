28b70a8cc5d8e1ca27a48655702ab9dada01cf0b
Che Tran <chelongtran@gmail.com>
2024-11-15 11:22:18 +0100

    Bug Fix. [frontend]. When draging from the canvas to the navbar it would draw lines. It now checks if point is in bounds

---
+++ app/src/main/java/com/example/dat068_tentamina/ui/DrawingScreen.kt
@@ -0,0 +1,53 @@
+package com.example.dat068_tentamina.ui
+
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.example.dat068_tentamina.model.Line
+import com.example.dat068_tentamina.viewmodel.TentaViewModel
+
+@Composable
+fun DrawingScreen(viewModel: TentaViewModel) {
+    androidx.compose.foundation.Canvas(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectDragGestures { change, dragAmount ->
+                change.consume()
+                val startPosition = change.position - dragAmount
+                val endPosition = change.position
+
+                // Ensure that both start and end positions are within the Canvas bounds
+                if (isInBounds(startPosition, size) && isInBounds(endPosition, size)) {
+                    viewModel.addLine(
+                        Line(
+                            start = startPosition,
+                            end = endPosition,
+                            strokeWidth = 2.dp
+                        )
+                    )
+                }
+            }
+        }
+
+    ) {
+        viewModel.lines.forEach { line ->
+            drawLine(
+                start = line.start,
+                end = line.end,
+                color = line.color,
+                strokeWidth = line.strokeWidth.toPx(),
+                cap = StrokeCap.Round
+            )
+        }
+    }
+}
+
+private fun isInBounds(point: Offset, canvasSize: IntSize): Boolean {
+    return point.x in 0f..canvasSize.width.toFloat() && point.y in 0f..canvasSize.height.toFloat()
+}
