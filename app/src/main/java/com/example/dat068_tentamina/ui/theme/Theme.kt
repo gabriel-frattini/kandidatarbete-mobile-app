857a4c53b6b7a1c8e97234ede3d3625b69a18225
Che Tran <chelongtran@gmail.com>
2024-11-15 11:22:46 +0100

    Refactor. [backend]. Moved folder

--- app/src/main/java/com/example/dat068_tentamina/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dat068_tentamina/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.dat068_tentamina.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
