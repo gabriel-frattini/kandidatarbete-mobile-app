1d8a43cbc6058f3252f0a9b7aa90084c0a610499
Che Tran <chelongtran@gmail.com>
2024-11-17 20:30:15 +0100

    Feat. [backend]. Added history to viewmodel

--- app/src/main/java/com/example/dat068_tentamina/viewmodel/TentaViewModel.kt
+++ app/src/main/java/com/example/dat068_tentamina/viewmodel/TentaViewModel.kt
@@ -1,19 +1,31 @@
 package com.example.dat068_tentamina.viewmodel
 
+import Stack
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.example.dat068_tentamina.model.Line
 
 class TentaViewModel {
     private val _lines = mutableStateListOf<Line>()
+    private val history = Stack<List<Line>>()
     val lines : SnapshotStateList<Line> get() = _lines
 
     fun addLine(line : Line) {
         _lines.add(line)
     }
     fun pop() {
-        if (_lines.isNotEmpty()) {
-            _lines.removeLast()
+        if (_lines.isNotEmpty() && history.isNotEmpty()) {
+            _lines.clear()
+            val previousState = history.getCurrValue()
+            if (previousState != null) {
+                _lines.addAll(previousState)
+            }
+            history.pop()
         }
     }
+    fun saveHistory() {
+        // toList to avoid it as a reference
+        history.append(_lines.toList())
+    }
+
 }
