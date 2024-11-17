16f284746ee9b6dd063df570d37311042efb6495
Che Tran <chelongtran@gmail.com>
2024-11-17 20:30:10 +0100

    Feat. [backend]. Added history to viewmodel

--- app/src/main/java/com/example/dat068_tentamina/ui/DrawingScreen.kt
+++ app/src/main/java/com/example/dat068_tentamina/ui/DrawingScreen.kt
@@ -1,5 +1,6 @@
 package com.example.dat068_tentamina.ui
 
+import android.util.Log
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -17,22 +18,28 @@ fun DrawingScreen(viewModel: TentaViewModel) {
     androidx.compose.foundation.Canvas(modifier = Modifier
         .fillMaxSize()
         .pointerInput(Unit) {
-            detectDragGestures { change, dragAmount ->
-                change.consume()
-                val startPosition = change.position - dragAmount
-                val endPosition = change.position
+            detectDragGestures(
+            onDragStart = { startPosition ->
+                viewModel.saveHistory()
 
-                // Ensure that both start and end positions are within the Canvas bounds
-                if (isInBounds(startPosition, size) && isInBounds(endPosition, size)) {
-                    viewModel.addLine(
-                        Line(
-                            start = startPosition,
-                            end = endPosition,
-                            strokeWidth = 2.dp
+            },
+            onDrag = { change, dragAmount ->
+                    change.consume()
+                    val startPosition = change.position - dragAmount
+                    val endPosition = change.position
+
+                    // Ensure that both start and end positions are within the Canvas bounds
+                    if (isInBounds(startPosition, size) && isInBounds(endPosition, size)) {
+                        viewModel.addLine(
+                            Line(
+                                start = startPosition,
+                                end = endPosition,
+                                strokeWidth = 2.dp
+                            )
                         )
-                    )
+                    }
                 }
-            }
+            )
         }
 
     ) {
