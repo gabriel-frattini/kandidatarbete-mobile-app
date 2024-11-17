6cd89fb38d8c175d959b5094217e9a79a5067df9
Che Tran <chelongtran@gmail.com>
2024-11-17 20:29:07 +0100

    Feat. [backend]. Implemented a stack

---
+++ app/src/main/java/com/example/dat068_tentamina/model/Stack.kt
@@ -0,0 +1,32 @@
+class Stack<T> {
+    private data class Node<T>(var data: T, var next: Node<T>? = null)
+
+    private var head: Node<T>? = null
+    private var size: Int = 0
+
+    fun append(value : T) {
+        if (head == null) {
+            head = Node(value)
+        } else {
+            var newNode = Node(value, head)
+            head = newNode
+        }
+        size++
+    }
+
+    fun pop() {
+        if (head != null) {
+            head = head?.next
+            size--
+        }
+    }
+
+    fun getCurrValue(): T? {
+        return head?.data
+    }
+
+    fun isNotEmpty() : Boolean {
+        return size != 0
+    }
+
+}
