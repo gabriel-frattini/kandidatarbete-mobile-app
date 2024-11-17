ed72aaa13678314fb7b012fe0f5c8dec4b24e134
Che Tran <chelongtran@gmail.com>
2024-11-17 20:46:42 +0100

    Feat. [backend]. Added history eraser and pen to toolbar

--- app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
+++ app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -15,6 +18,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.dat068_tentamina.R
 import com.example.dat068_tentamina.viewmodel.TentaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,7 +35,16 @@ fun Overlay(viewModel: TentaViewModel) {
                 }
             },
             actions = {
-                // RowScope here, so these icons will be placed horizontally
+                IconButton(onClick = {
+
+                }) {
+                    Icon(painter = painterResource(id = R.drawable.eraser), contentDescription = "eraser", modifier = Modifier.size(30.dp))
+                }
+                IconButton(onClick = {
+
+                }) {
+                    Icon(Icons.Filled.Edit, contentDescription = "erase")
+                }
                 IconButton(onClick = {
                     viewModel.pop()
                 }) {
