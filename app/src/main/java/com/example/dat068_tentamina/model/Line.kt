e88142b0a0ec9fdda2a2c605f6aa8c7f65077b0e
Che Tran <chelongtran@gmail.com>
2024-11-18 23:22:24 +0100

    Fix. [backend]. Line now implements canvas objext

--- app/src/main/java/com/example/dat068_tentamina/model/Line.kt
+++ app/src/main/java/com/example/dat068_tentamina/model/Line.kt
@@ -1,15 +1,29 @@
 package com.example.dat068_tentamina.model
 
+import android.graphics.Canvas
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-data class Line(
+
+
+data class Line (
     val start: Offset,
     val end: Offset,
     var color: Color = Color.Black,
     var strokeWidth: Dp = 1.dp,
     var cap: StrokeCap = StrokeCap.Round
-)
+) : CanvasObject {
+    override fun draw(drawScope: DrawScope) {
+        drawScope.drawLine(
+            start = start,
+            end = end,
+            color = color,
+            cap = cap ,
+            strokeWidth = strokeWidth.value
+        )
+    }
+}
