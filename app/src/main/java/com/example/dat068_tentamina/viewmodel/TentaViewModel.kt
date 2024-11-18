670ba1fb262cb5180b29bba1ccd6f714551ec782
Che Tran <chelongtran@gmail.com>
2024-11-18 23:23:14 +0100

    Feat. [backend]. Textmode

--- app/src/main/java/com/example/dat068_tentamina/viewmodel/TentaViewModel.kt
+++ app/src/main/java/com/example/dat068_tentamina/viewmodel/TentaViewModel.kt
@@ -1,34 +1,43 @@
 package com.example.dat068_tentamina.viewmodel
 
 import Stack
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.dp
+import com.example.dat068_tentamina.model.CanvasObject
 import com.example.dat068_tentamina.model.Line
 
 class TentaViewModel {
-    private val _lines = mutableStateListOf<Line>()
-    private val history = Stack<List<Line>>()
-    public var strokeWidth = 2.dp
-    public var eraserWidth = 6.dp
-    public var eraser = false
-    val lines : SnapshotStateList<Line> get() = _lines
+    private val _objects = mutableStateListOf<CanvasObject>()
+    private val history = Stack<List<CanvasObject>>()
+    var textMode = mutableStateOf(false)
+    var strokeWidth = 2.dp
+    var eraserWidth = 6.dp
+    var eraser = false
+    val objects : SnapshotStateList<CanvasObject> get() = _objects
 
-    fun addLine(line : Line) {
-        _lines.add(line)
+    fun addObject(obj : CanvasObject) {
+        _objects.add(obj)
     }
     fun pop() {
-        if (_lines.isNotEmpty()) {
-            _lines.clear()
+        if (_objects.isNotEmpty()) {
+            _objects.clear()
             val previousState = history.getCurrValue()
             if (previousState != null) {
-                _lines.addAll(previousState)
+                _objects.addAll(previousState)
             }
             history.pop()
         }
     }
     fun saveHistory() {
         // toList to avoid it as a reference
-        history.append(_lines.toList())
+        history.append(_objects.toList())
     }
+
+
 }
