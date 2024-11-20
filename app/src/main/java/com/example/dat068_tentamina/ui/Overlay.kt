6ae24f44254c07cfe280c51a00d9c3f68368ea70
saadih <120604473+saadih@users.noreply.github.com>
2024-11-20 21:59:48 +0100

    Feat. Changed questions to mutable
    This allows for dynamic updates for the questions; recomposing

--- app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
+++ app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
@@ -1,66 +1,49 @@
 package com.example.dat068_tentamina.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentComposer
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.dat068_tentamina.R
 import com.example.dat068_tentamina.viewmodel.TentaViewModel
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.collectAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -68,6 +51,7 @@ fun Overlay(viewModel: TentaViewModel) {
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
@@ -81,7 +65,7 @@ fun Overlay(viewModel: TentaViewModel) {
         Scaffold(
             topBar = {
                 TopAppBar(
-                    title = { Text("Question ${viewModel.getCurrentQuestionNr()}") },
+                    title = { Text("Question ${viewModel.currentQuestion.intValue} ") },
                     navigationIcon = {
                         IconButton(onClick = { scope.launch {
                             drawerState.apply {
@@ -190,7 +174,7 @@ fun MenuScreen(modifier: Modifier = Modifier, viewModel: TentaViewModel){
                 Button(
                     onClick = {
                         println("Bytte fråga till $key ")
-                        viewModel.changedQuestion(key)
+                        viewModel.changeQuestion(key)
                               },
                     modifier = Modifier
                         .padding(10.dp)
--- app/src/main/java/com/example/dat068_tentamina/viewmodel/TentaViewModel.kt
+++ app/src/main/java/com/example/dat068_tentamina/viewmodel/TentaViewModel.kt
@@ -1,38 +1,31 @@
 package com.example.dat068_tentamina.viewmodel
 
 import Stack
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.dp
 import com.example.dat068_tentamina.model.CanvasObject
-import com.example.dat068_tentamina.model.Line
-import com.example.dat068_tentamina.ui.DrawingScreen
-import com.example.dat068_tentamina.ui.ExamScreen
-import com.example.dat068_tentamina.ui.Overlay
 
 class TentaViewModel {
     private val _objects = mutableStateListOf<CanvasObject>()
     private val history = Stack<List<CanvasObject>>()
+    private var ob: List<CanvasObject>? = emptyList()
+
     var textMode = mutableStateOf(false)
     var strokeWidth = 2.dp
     var eraserWidth = 6.dp
     var eraser = false
-    val objects : SnapshotStateList<CanvasObject> get() = _objects
+    var currentQuestion = mutableIntStateOf(1)
+    val objects: SnapshotStateList<CanvasObject> get() = _objects
     val questions = mutableMapOf<Int, List<CanvasObject>>()
-    private var currentQuestion = 1
-    private var ob : List<CanvasObject>? = emptyList()
 
-
-    fun addObject(obj : CanvasObject) {
+    fun addObject(obj: CanvasObject) {
         objects.add(obj)
-        questions.put(currentQuestion,_objects)
+        questions[currentQuestion.intValue] = _objects.toList()
     }
+
     fun pop() {
         if (_objects.isNotEmpty()) {
             _objects.clear()
@@ -43,28 +36,25 @@ class TentaViewModel {
             history.pop()
         }
     }
+
     fun saveHistory() {
-        // toList to avoid it as a reference
         history.append(_objects.toList())
     }
-    fun addQuestions()
-    {
-        var i = 1
-        while(i<=10)
-        {
-            //just adding empty first
-            questions.put(i, emptyList())
-            i++
+
+    fun addQuestions() {
+        for (i in 1..10) {
+            questions[i] = emptyList()
         }
     }
-    fun changedQuestion(qNr:Int)
-    {
-        currentQuestion = qNr
-        // hur ska jag ändra värdet på _object för varje page???
-        ob =  questions.get(currentQuestion)
-    }
-    fun getCurrentQuestionNr(): Int
-    {
-        return currentQuestion
+
+    fun changeQuestion(qNr: Int) {
+        currentQuestion.intValue = qNr
+
+        /* ska vi göra detta eller? osäker om vi ska börja jobba på att spara svaren o.s.v.
+         questions[currentQuestion.intValue] = _objects.toList()
+        _objects.clear()
+        val newObjects = questions[qNr] ?: emptyList()
+        _objects.addAll(newObjects)
+         */
     }
 }
