679bfb58d263c3f53bd2386f5928db5b4457bdd7
aliceemanuelsson <aliceema@chalmers.se>
2024-11-20 11:40:48 +0100

    Feat [Frontend] Added Navigation Drawer to TopApp

--- app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
+++ app/src/main/java/com/example/dat068_tentamina/ui/Overlay.kt
@@ -1,65 +1,138 @@
 package com.example.dat068_tentamina.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.dat068_tentamina.R
 import com.example.dat068_tentamina.viewmodel.TentaViewModel
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Overlay(viewModel: TentaViewModel) {
-    Column {
-        TopAppBar(
-            title = { Text("Simple TopAppBar") },
-            navigationIcon = {
-                IconButton(onClick = { /* doSomething() */ }) {
-                    Icon(Icons.Filled.Menu, contentDescription = null)
-                }
-            },
-            actions = {
-                IconButton(onClick = {
-                    viewModel.textMode.value = true
-                }) {
-                    Icon(painter = painterResource(id = R.drawable.text), contentDescription = "text", modifier = Modifier.size(25.dp))
-                }
-                IconButton(onClick = {
-                }) {
-                    SizePicker(viewModel,"eraser")
-                }
-                IconButton(onClick = {}) {
-                    SizePicker(viewModel)
-                }
-                IconButton(onClick = {
-                    viewModel.pop()
-                }) {
-                    Icon(Icons.Filled.ArrowBack, contentDescription = "Localized description")
-                }
-                IconButton(onClick = { /* doSomething() */ }) {
-                    Icon(Icons.Filled.ArrowForward, contentDescription = "Localized description")
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .background(Color.LightGray)
+                        .fillMaxHeight()
+                        .requiredWidth(500.dp)
+                ) {
+                    IconButton(
+
+                        onClick = {/* TODO : lägg in användar info*/},
+
+                    ) {
+                        Text("User Information")
+                        Icon(Icons.Filled.AccountCircle, contentDescription = "user information")
+                    }
+                    IconButton(onClick = {/* TODO : lägg in svar sidorna*/}) { }
+
                 }
             }
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Simple TopAppBar") },
+                    navigationIcon = {
+                        IconButton(onClick = { scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        } }) {
+                            Icon(Icons.Filled.Menu, contentDescription = null)
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            viewModel.textMode.value = true
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.text),
+                                contentDescription = "text",
+                                modifier = Modifier.size(25.dp)
+                            )
+                        }
+                        IconButton(onClick = {
+                        }) {
+                            SizePicker(viewModel, "eraser")
+                        }
+                        IconButton(onClick = {}) {
+                            SizePicker(viewModel)
+                        }
+                        IconButton(onClick = {
+                            viewModel.pop()
+                        }) {
+                            Icon(
+                                Icons.Filled.ArrowBack,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(
+                                Icons.Filled.ArrowForward,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    }
+                )
+            },
         )
-        Box(modifier = Modifier.weight(1f).fillMaxSize()) {
-            DrawingScreen(viewModel)
+        { contentPadding ->
+            ExamScreen(modifier = Modifier.padding(contentPadding), viewModel)
         }
     }
-
 }
+@Composable
+        fun ExamScreen(modifier: Modifier = Modifier, viewModel: TentaViewModel ) {
+            Column {
+                Box(modifier = Modifier.weight(1f).fillMaxSize()) {
+                    DrawingScreen(viewModel)
+                }
+            }
+        }
