858821fd8138d10536521d9cbca336de7febe5d9
chetran <chelongtran@gmail.com>
2024-12-02 00:12:36 +0100

    Refactor. [backend] changed imports

--- app/src/main/java/com/example/dat068_tentamina/model/Line.kt
+++ app/src/main/java/com/example/dat068_tentamina/model/Line.kt
@@ -1,14 +1,12 @@
 package com.example.dat068_tentamina.model
 
-import android.graphics.Canvas
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-
-
 
 data class Line (
     val start: Offset,
@@ -18,12 +16,13 @@ data class Line (
     var cap: StrokeCap = StrokeCap.Round
 ) : CanvasObject {
     override fun draw(drawScope: DrawScope) {
+        // Pass parameters in the correct order without named arguments
         drawScope.drawLine(
-            start = start,
+            start = start,             // Correct order and no named argument
             end = end,
             color = color,
-            cap = cap ,
-            strokeWidth = strokeWidth.value
+            strokeWidth = strokeWidth.value,
+            cap = cap
         )
     }
 }
