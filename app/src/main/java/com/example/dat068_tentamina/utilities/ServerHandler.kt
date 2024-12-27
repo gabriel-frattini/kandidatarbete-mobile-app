bd82f66659217789dc3c4ce42faf45776e5c157c
aliceemanuelsson <aliceema@chalmers.se>
2024-12-27 14:08:43 +0100

    Fix [backend] The backup is saved on the actual SD card by fixing bug.

--- app/src/main/java/com/example/dat068_tentamina/utilities/ServerHandler.kt
+++ app/src/main/java/com/example/dat068_tentamina/utilities/ServerHandler.kt
@@ -14,7 +14,7 @@ import java.io.IOException
 
 class ServerHandler {
     private val SCHEME = "http"
-    private val HOST = "192.168.0.104"
+    private val HOST = "10.0.1.45"
     private val PORT = 3000
     private val client = OkHttpClient()
 
